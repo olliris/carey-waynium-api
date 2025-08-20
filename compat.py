70c48623ede1b2c73ea171cbe21b5b9405954c94
Olliris <166351801+olliris@users.noreply.github.com>
2025-08-20 21:43:30 +0200

    Create compat.py

---
+++ compat.py
@@ -0,0 +1,5 @@
+export WEBHOOK_API_KEYS="1"
+export WAYNIUM_API_URL="https://stage-gdsapi.waynium.net/api-externe/set-ressource"
+export WAYNIUM_API_KEY="abllimousines"
+export WAYNIUM_API_SECRET="be5F47w72eGxwWe8EAZe9Y4vP38g2rRG"
+export WAYNIUM_AUTH_MODE="HEADERS"   # ou BASIC/HMAC si Waynium l’exige
